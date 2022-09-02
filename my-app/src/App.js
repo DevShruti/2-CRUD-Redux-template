953237f3dd2757df7db2e2d479934a424605371b
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-09-02 17:07:32 -0500

    names showing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import "./App.css";
-
+import { useSelector } from "react-redux";
 
 
 function App() {
-
+  const userList = useSelector((state) => state.users.value);
   return (
-      <div className="addUser">
-        <input
-          type="text"
-          placeholder="Name..."
-        
-        />
-        <input
-          type="text"
-          placeholder="Username..."
-          
-        />
-        <button>
-          Add User
-        </button>
-      </div>
+    
+    
+    <div className="displayUsers">
+      {userList.map((user) => {
+        return (
+          <div>
+            <h1> {user.name}</h1>
+            </div>
   );
-}
+})};
+</div>
+  )}
 
 export default App;
