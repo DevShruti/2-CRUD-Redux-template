57ff970c4dd8e90340d76210be12aec0277f08e6
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-09-02 16:26:29 -0500

    start

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./App.css";
+
+
+
+function App() {
+
+  return (
+      <div className="addUser">
+        <input
+          type="text"
+          placeholder="Name..."
+        
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          
+        />
+        <button>
+          Add User
+        </button>
+      </div>
+  );
+}
+
+export default App;
