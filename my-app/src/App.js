2d5ea266b00d9b0ef225635c16013593ebe95c61
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-09-03 10:25:45 -0500

    input and list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,21 @@ import { useSelector } from "react-redux";
 function App() {
   const userList = useSelector((state) => state.users.value);
   return (
-    
+    <div className="addUser">
+        <input
+          type="text"
+          placeholder="Name..."
+        
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          
+        />
+        <button>
+          Add User
+        </button>
+      
     
     <div className="displayUsers">
       {userList.map((user) => {
@@ -16,6 +30,7 @@ function App() {
             </div>
   );
 })};
+</div>
 </div>
   )}
 
